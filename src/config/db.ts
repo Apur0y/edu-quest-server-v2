dbdc2cfe7f80f6c7dc3edd3cea88731b914c4fac
Apur0y <apuroy2785@gmail.com>
2026-06-07 11:47:11 +0600

    Change prisma client import

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
+import { PrismaClient } from "../../generated/prisma";
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../../generated/prisma/index.js';
 
 const connectionString = process.env.DATABASE_URL || '';
 
